fbd4f2404672cc9782db64ac5ac29e71eaffc8b3
ViShal69x <tk5762996@gmail.com>
2023-02-13 17:03:29 +0530

    liber: TequilaOS bringup

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/LineageOS/android_system_qcom system/qcom
+git clone https://github.com/LineageOS/android_hardware_motorola hardware/motorola
+git clone https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
